721b3a9f07bd4a6d3599d857d3a287b3db27908d
noorihokayat-droid <noorihokayat@gmail.com>
2026-09-26 18:54:41 +0530

    python

---
+++ data_analytics/module-3-python/corepython/1-examples.py
@@ -0,0 +1,9 @@
+a=10
+b=20
+c="hi"
+d='hello world'
+e='''
+hello ,
+this is my 1st example of multi line string in python.
+'''
+print(e)
---
+++ data_analytics/module-3-python/corepython/2-single-line-comment.py
@@ -0,0 +1,7 @@
+# Write a program to print user name take input from user
+# using input() method
+# name='noori'
+# print dynamic values of users
+
+name = input("enter your name: ")
+print("hello " + name + " welcome to python programming")
